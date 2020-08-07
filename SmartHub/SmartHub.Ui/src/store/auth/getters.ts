b9f899747ad69edca8ec2232be5373de268fe868
Maximilian Stümpfl <maximilian2304@t-online.de>
2020-08-07 14:09:18 +0200

    AB#126: remove not needed getters

--- SmartHub/SmartHub.Ui/src/store/auth/getters.ts
+++ SmartHub/SmartHub.Ui/src/store/auth/getters.ts
@@ -1,19 +1,8 @@
 import { GetterTree } from 'vuex';
-import { AuthResponse } from '@/types/types';
 import { RootState, AuthState } from '@/store/index.types';
 
 // Getter Types
-export type AuthGetters = {
-  getRole(state: AuthState): string[] | undefined;
-  getAuthResponse(state: AuthState): AuthResponse | null;
-};
+export type AuthGetters = {};
 
 // Define Getters
-export const getters: GetterTree<AuthState, RootState> & AuthGetters = {
-  getRole(state: AuthState): string[] | undefined {
-    return state.authResponse?.roles;
-  },
-  getAuthResponse(state: AuthState): AuthResponse | null {
-    return state.authResponse;
-  }
-};
+export const getters: GetterTree<AuthState, RootState> & AuthGetters = {};
--- SmartHub/SmartHub.Ui/src/store/home/getters.ts
+++ SmartHub/SmartHub.Ui/src/store/home/getters.ts
@@ -1,14 +1,7 @@
 import { GetterTree } from 'vuex';
-import { Home } from '@/types/types';
 import { HomeState, RootState } from '@/store/index.types';
 
 // Getter Types
-export type HomeGetters = {
-  getHome(state: HomeState): Home | null;
-};
+export type HomeGetters = {};
 
-export const getters: GetterTree<HomeState, RootState> = {
-  getHome(state: HomeState): Home | null {
-    return state.home;
-  }
-};
+export const getters: GetterTree<HomeState, RootState> = {};
