8a263b6d4b6a3e65ce9002aa61f1749e15df76a2
Maximilian Stümpfl <maximilian2304@t-online.de>
2020-08-11 10:05:08 +0200

    AB#126: add component file for tables

---
+++ SmartHub/SmartHub.Ui/src/components/widgets/LiveTable.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Table
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  props: {},
+  setup(props) {
+    return {};
+  }
+});
+</script>
+
+<style scoped></style>
