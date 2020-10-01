f85b84cc8a0de1baf72fa572ab8ffe2d2e945bf7
Maximilian Stümpfl <maximilian2304@t-online.de>
2020-10-01 15:15:43 +0200

    AB#140: Add GetAll devices logic

---
+++ SmartHub/SmartHub.Api/Controllers/DeviceController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartHub.Application.UseCases.Entity.Devices.Read;
+using SmartHub.Application.UseCases.Entity.Groups;
+
+namespace SmartHub.Api.Controllers
+{
+    public class DeviceController : BaseController
+    {
+        /// <summary>
+        /// Get all devices
+        /// </summary>
+        /// <returns>List of devices</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await Mediator.Send(new DeviceGetQuery()));
+        }
+
+        /// <summary>
+        /// Create a device
+        /// </summary>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Post([FromBody] GroupCreateCommand value)
+        {
+            var response = await Mediator.Send(value).ConfigureAwait(false);
+            return CreatedAtAction("Post", response.Data.Id, response);
+        }
+    }
+}
--- SmartHub/SmartHub.Application/UseCases/Entity/Devices/DeviceDto.cs
+++ SmartHub/SmartHub.Application/UseCases/Entity/Devices/DeviceDto.cs
@@ -1,10 +1,34 @@
-﻿using SmartHub.Application.Common.Mappings;
+﻿using AutoMapper;
+using SmartHub.Application.Common.Mappings;
+using SmartHub.Domain.Common.Enums;
 using SmartHub.Domain.Entities;
+using SmartHub.Domain.Entities.Devices;
 
 namespace SmartHub.Application.UseCases.Entity.Devices
 {
     public class DeviceDto : BaseDto, IMapFrom<Device>
     {
+        public IpAddress Ip { get; set; }
 
+        public Company Company { get; set; }
+
+        public string PrimaryConnection { get; set; }
+
+        public string SecondaryConnection { get; set; }
+
+        public string PluginName { get; set; } // Equals the Name Property in the IPlugin
+        public string PluginTypes { get; set; }// Equals the PluginType Property in the IPlugin
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Device, DeviceDto>()
+                .ForMember(x => x.PluginTypes,
+                    x =>
+                        x.MapFrom(d => d.PluginTypes.ToString()))
+                .ForMember(x => x.PrimaryConnection, x =>
+                    x.MapFrom(d => d.PrimaryConnection.ToString()))
+                .ForMember(x => x.SecondaryConnection, x =>
+                    x.MapFrom(d => d.SecondaryConnection.ToString()));
+        }
     }
 }
