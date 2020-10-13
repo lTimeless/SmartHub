4c2733148e884c8b8c91e8f9e8bd044fc1dcb3fa
Maximilian Stümpfl <maximilian2304@t-online.de>
2020-10-13 21:05:21 +0200

    add commecnt for some QoL

--- SmartHub/SmartHub.Application/Common/Behaviours/RequestLoggerBehaviour.cs
+++ SmartHub/SmartHub.Application/Common/Behaviours/RequestLoggerBehaviour.cs
@@ -1,8 +1,11 @@
-﻿using System.Threading;
+﻿using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Serilog;
 using SmartHub.Application.Common.Models;
+using SmartHub.Application.UseCases;
+using SmartHub.Domain.Entities;
 
 namespace SmartHub.Application.Common.Behaviours
 {
@@ -22,8 +25,25 @@ namespace SmartHub.Application.Common.Behaviours
 
             _logger.Information("SmartHub Request: {Name} - {@UserName} - {@Request}",
                 name, _currentUser.User == null ? _currentUser.RequesterName : _currentUser.User.UserName, request);
+            //var response = next();
+            // die respone ist mein Response class mit den daten darain
+            // von dem kann ich dann erfahren ob die request erfolgreich war oder nicht
+            // also response nur als Response casten und das wars
 
-            return await next();
+            // wenn currentUser == system dann schau im request nach dem Feld "UserName" und wenn das auch null ist dann
+            // ist der request anonym gewesen
+
+            var response = await next();
+            var successProp = response?.GetType().GetProperty("Success");
+            var successMessage = response?.GetType().GetProperty("Message");
+
+            // dann Logstatemant aufbauen:
+            // [Datetime] {Username} {Message} [ExecutionTime]
+            // die ganze logic dann in "requestPerformanceBehaviour" packen
+            // dann hier publishen als Event und dann der dispatcher baut daraus dann ein richtiges Event object
+            //
+
+            return response;
         }
     }
 }
