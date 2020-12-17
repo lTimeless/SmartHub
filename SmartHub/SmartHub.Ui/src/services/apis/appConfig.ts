f9fb3dae110895dce40440ae699aecb92cf57264
Maximilian Stümpfl <maximilian2304@t-online.de>
2020-12-17 15:46:55 +0100

    Remove unused services and axios

--- SmartHub/SmartHub.Ui/src/services/apis/appConfig.ts
+++
@@ -1,7 +0,0 @@
-import { api } from '@/router/axios/axios';
-import { Payload, AppConfig } from '@/types/types';
-
-const API_INIT_URL = 'api/Init';
-
-export const getAppConfig = (): Promise<Payload<AppConfig>> =>
-  api.get<Payload<AppConfig>>(API_INIT_URL).then((res) => res.data);
