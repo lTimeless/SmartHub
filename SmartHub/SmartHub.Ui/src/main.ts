db20b0c49d25c9ae03566e5fffe65500b8826396
Maximilian Stümpfl <maximilian2304@t-online.de>
2021-01-02 22:33:48 +0100

    codacy fixes

--- SmartHub/SmartHub.Ui/src/main.ts
+++ SmartHub/SmartHub.Ui/src/main.ts
@@ -4,7 +4,6 @@ import App from './App.vue';
 import router from './router';
 import { store } from './store';
 import '@/assets/styles/tailwind.css';
-import '@/assets/styles/main.css';
 import { DefaultApolloClient } from '@vue/apollo-composable';
 import { apolloClient } from './apollo';
 import IconPlugin from '@/plugins/IconPlugin';
