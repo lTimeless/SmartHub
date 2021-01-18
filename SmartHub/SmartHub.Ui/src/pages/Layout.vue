573ee439c45eae669158386e758983be7f124464
Maximilian Stümpfl <maximilian2304@t-online.de>
2021-01-18 20:30:55 +0100

    try to make it more responsive
    🤢stupid scrollbehviour

--- SmartHub/SmartHub.Ui/src/pages/Layout.vue
+++ SmartHub/SmartHub.Ui/src/pages/Layout.vue
@@ -39,11 +39,8 @@ export default defineComponent({
     <!-- Sidebar-->
     <AppSidebar v-if="!showMobileSidebar" />
     <!-- Main View-->
-    <div
-      class="absolute transition md:relative flex flex-auto h-screen bg-gray-200 w-full overflow-x-hidden background"
-      :class="[showMobileSidebar ? 'transform translate-x-48' : '']"
-    >
-      <div class="flex flex-col w-full m-5 px-3 z-10">
+    <div class="main background" :class="[showMobileSidebar ? 'transform transition translate-x-48' : '']">
+      <div class="flex flex-col w-full m-2 md:m-5 md:px-3 z-10">
         <Navbar />
         <router-view v-slot="{ Component }">
           <transition
@@ -65,5 +62,8 @@ export default defineComponent({
 <style scoped>
 .transition {
   transition: 0.3s transform cubic-bezier(0, 0.12, 0.14, 1);
+}
+.main {
+  @apply absolute transition md:relative flex flex-auto h-screen w-full overflow-y-auto;
 }
 </style>
